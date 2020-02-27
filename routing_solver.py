e9e7d3d53acd14fbf9fd466befde4e70f8fd7d81
John <john.r.muse@gmail.com>
2020-02-27 12:04:30 -0500

    Seperation of Code                                 Eventually this should be the place that code for solving the problem (comput routes and schedules for each vehicle) and __main_for starting the code.

--- routing_solver.py
+++ routing_solver.py
@@ -1,3 +1,6 @@
 from __future__ import print_function
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
+
+if __name__ == '__main__':
+    pass
